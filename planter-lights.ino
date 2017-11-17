9196d7faeb17575398e0e84be9aa619d1a443d1a
John Griffiths <john@johnantoni.com>
2017-11-17 10:49:17 -0500

    Add fan control, increase loop interval, add comments

--- planter-lights.ino
+++ planter-lights.ino
@@ -1,16 +1,19 @@
 // NeoPixel Planter Lights (c) 2017 John Griffiths
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
+// Include necessary libraries
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
+// Set which digital pin we will use for the fan
+const int FANPIN       10
+
+// Which pin on the arduino is connected to the neopixel grid?
 #define PIN            6
 
-// How many NeoPixels are attached to the Arduino?
+// How many neopixels are on the grid?
 #define NUMPIXELS      40
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
@@ -19,15 +22,24 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // setup light sensor and serial
+  // Setup light sensor
   pinMode(0, INPUT);
+  
+  // Activate serial console for debug output
   Serial.begin(9600);
 
-  // setup and run neopixel lights
-  pixels.begin(); // This initializes the NeoPixel library.
+  // Initialize neopixel library
+  pixels.begin();
+  
+  // Setup pin for external fan
+  pinMode(FANPIN, OUTPUT);
 }
 
 void lightOn() {
+  // Turn on FAN
+  digitalWrite(FANPIN, HIGH);
+  
+  // Setup grid
   int i;
   int pixelPoint[] = {0,0,0,0,0,0,0,0,
                   1,1,1,1,1,1,1,1,
@@ -36,21 +48,27 @@ void lightOn() {
                   0,0,0,0,0,0,0,0,
                   1,1,1,1,1,1,1,1};
 
+  // Turn on each neopixel based on the pixelPoint integer, 1 = Red, 0 = Blue
   for (i = 0; i < 40; i++) {
-
+    
+    // Set color
     if (pixelPoint[i] == 1) {
       pixels.setPixelColor(i, pixels.Color(255,0,0));
     } else {
       pixels.setPixelColor(i, pixels.Color(0,0,255));
     }
 
+    // Turn on neopixel
     pixels.show();
   }
 }
 
 void lightOff() {
+  // Turn off FAN
+  digitalWrite(FANPIN, LOW);
+  
+  // Turn off all neopixels
   int i;
-
   for (i = 0; i < 40; i++) {
     pixels.setPixelColor(i, pixels.Color(0,0,0));
     pixels.show();
@@ -58,9 +76,13 @@ void lightOff() {
 }
 
 void loop() {
+  // Read light sensor value
   int v = analogRead(0);
+  
+  // Send light sensor value to console
   Serial.println(v);
 
+  // If lightsensor > 300, turn on lights, otherwise keep them off
   if (v > 300) {
     Serial.println("dark");
     lightOn();
@@ -69,6 +91,7 @@ void loop() {
     lightOff();
   }
 
-  delay(1000);
+  // Loop interval
+  delay(2000);
 }
 
